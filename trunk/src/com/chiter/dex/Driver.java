3d13f3288bce4f8693ebd3bb090d517bda9d2b26
chang.kerry <chang.kerry@74d73090-cc81-1ab9-6136-82eb78f6bd55>
2012-05-20 02:58:30 +0000

    update to read from corpus and a search string from command line

--- trunk/src/com/chiter/dex/Driver.java
+++ trunk/src/com/chiter/dex/Driver.java
@@ -1,53 +1,46 @@
 package com.chiter.dex;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Driver {
-
-
+	
 	public static void main (String[] args) {
 
-		//useless comment
-		String searchString = "What is my list of grocery ?";
-
+		String searchString = args[0];
+		
 		List<String> documents = new ArrayList<String>();
+		try {
+			FileInputStream fstream = new FileInputStream("textfile.txt");
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-		String testDocuments1 = "Remember my car mechanic's number : 206-555-2222";
-		String testDocuments2 = "Just met this girl named Jane";
-		String testDocuments3 = "Here is a song that I like : Baby baby";
-		String testDocuments4 = "Remind me to buy eggs and bagels for my grocery list";
-		String testDocuments5 = "Add ham to my grocery list";
-		String testDocuments6 = "Add bread to the grocery list";
-		String testDocuments7 = "I need toilet paper in my grocery";
-		
-		documents.add(testDocuments1);
-		documents.add(testDocuments2);
-		documents.add(testDocuments3);
-		documents.add(testDocuments4);
-		documents.add(testDocuments5);
-		documents.add(testDocuments6);
-		documents.add(testDocuments7);
-		
+			String strLine;
+			//Read File Line By Line
+			while ((strLine = br.readLine()) != null)   {
+				documents.add(strLine);
+			}
+			//Close the input stream
+			in.close();
+		} catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
 
 		TfIdfManager manager = new TfIdfManager();
 		List<TfIdfResponse> responseList = manager.calculateTfIdf(searchString, documents);
-		for (TfIdfResponse response : responseList) {
-			System.out.println(response);
-		}
-		
+
 		// sort this thing
 		Collections.sort(responseList);
-		
+
 		System.out.println("---------------------------------------------------------");
 		// TODO: return results
 		for (TfIdfResponse response : responseList) {
 			System.out.println(response);
 		}
-		
-		
 	}
-	
-
 }
