57abbbdbdeb8a0b702e70390dc8baa5dd34e677f
iter.dan <iter.dan@74d73090-cc81-1ab9-6136-82eb78f6bd55>
2012-06-24 01:23:45 +0000

    made driver go in infinite look and take input from user and from command line to ease testing.

--- trunk/src/com/chiter/dex/Driver.java
+++ trunk/src/com/chiter/dex/Driver.java
@@ -1,6 +1,7 @@
 package com.chiter.dex;
 
 import java.util.List;
+import java.util.Scanner;
 
 import com.chiter.dex.tfidf.TfIdfManager;
 import com.chiter.dex.tfidf.TfIdfResponse;
@@ -9,20 +10,29 @@ public class Driver {
 	
 	public static void main (String[] args) {
 		//TODO: Change to get searchString from file
-		//TODO: Do to do 
-		if (args.length <= 0) {
-			System.out.println("Enter a search string via command line arguments");
-			System.exit(1);
+		//TODO: Do to do
+		String searchString;
+		
+		while(true)
+		{
+			if (args.length <= 0) {
+				System.out.println("Enter a search string");
+				//System.exit(1);
+				Scanner in = new Scanner(System.in);
+				searchString = in.nextLine();
+			}
+			else{
+				searchString = args[0];
+			}
+			
+			TfIdfManager tfIdfManager = new TfIdfManager("corpora/corpus7", searchString);
+			tfIdfManager.execute();
+			
+			// print non zero tfidf results
+			List<TfIdfResponse> nonZeroResponseList = tfIdfManager.getSortedNonZeroResults();
+			
+			Driver.printResponse(nonZeroResponseList);
 		}
-		String searchString = args[0];
-		
-		TfIdfManager tfIdfManager = new TfIdfManager("corpora/corperaplusplus.txt", searchString);
-		tfIdfManager.execute();
-		
-		// print non zero tfidf results
-		List<TfIdfResponse> nonZeroResponseList = tfIdfManager.getSortedNonZeroResults();
-		
-		Driver.printResponse(nonZeroResponseList);
 	}
 	
 	private static void printResponse(List<TfIdfResponse> responseList) 
